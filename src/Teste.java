d646fc62be5a42ec956868d63b05bdf524b49a9b
ricardoF <ricardo.frozza71@gmail.com>
2014-08-28 21:53:00 -0300

    projeto eclipse

--- src/Teste.java
+++ src/Teste.java
@@ -1,4 +1,14 @@
 
 public class Teste {
-
+/* enviar 
+ * 1- git status
+ * 2- git add . ou arquivo
+ * 3- git commit -m "comentario"
+ * 5- git push origin master (envia)
+ * 
+ * receber
+ * 1- get pull origin master
+ */
+	
+	 
 }
